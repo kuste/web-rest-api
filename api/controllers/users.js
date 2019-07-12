a3a87d9b9afb22e52c676e0df1ca5afa8398fbfd
ivica.kustura@rathmann.hr <kuuste@gmail.com>
2019-07-12 16:35:50 +0200

    build

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose")
 const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
-
 const User = require("../models/user")
 
 exports.user_signup = (req, res, next) => {
