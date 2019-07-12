582418d30a72eb2a1c463625ff5b2ec76a22393d
ivica.kustura@rathmann.hr <kuuste@gmail.com>
2019-07-12 16:43:25 +0200

    aaa

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -66,7 +66,7 @@ exports.user_login = (req, res, next) => {
               email: user[0].email,
               userId: user[0]._id
             },
-            process.env.JWT_KEY,
+            "KOBIeayKqh5UXNpOMyOUAry4WK1NjopjukFokAnkssXYpiKJki1TFi-JQTkBN45n1L2eHAX1WpuB-k1Oc7Zq9mn-MRPDHEHdu9-jKRVreXZ04ZG9cnu7HTzWVkNMqqcYcs--qny623yEVsScDOb-aJ8QAIDWlsCASshp4BD_GoqkX25ml7T5bNB3lu5aMkxlQVmdcnhX3B_OqlSZJmeWi24ydyAkuqFvOJAES4gPuu-HQPVzJk-QcVF7zYJOfUEXLA6T5N14i9Z4N6X8366R6NV8rPpNY2861QAjIyKQvpF_FtxskytWJ_oPMIukpy940NwbUWjusik4_rSqKR0VoQ",
             {
               expiresIn: "1h"
             }
