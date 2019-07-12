2ac3d6a6ef04af81c60f44500c8303877f613b22
ivica.kustura@rathmann.hr <kuuste@gmail.com>
2019-07-12 16:04:44 +0200

    com

--- app.js
+++ app.js
@@ -3,36 +3,30 @@ const app = express()
 const morgan = require("morgan")
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
-const cors = require("cors")
 const userRoutes = require("./api/routes/user")
 const postsRoutes = require("./api/routes/posts")
 
 const uri = "mongodb+srv://kuste:pass123456@node-rest-api-7y9da.mongodb.net/test?retryWrites=true&w=majority"
-
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,content-type, Content-Length, Accept, Authorization")
+  res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET")
+  if (req.method === "OPTIONS") {
+    return res.status(200).json({})
+  }
+  next()
+})
 mongoose.connect(uri, {
   useNewUrlParser: true,
   useCreateIndex: true
 })
 
 mongoose.Promise = global.Promise
-
 app.use(morgan("dev"))
 app.use("/uploads", express.static("uploads"))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-/* app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,content-type, Accept, Authorization")
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET")
-    return res.status(200).json({})
-  }
-  next()
-})
- */
-app.use(cors())
-app.options('*', cors())
 // Routes which should handle requests
 app.use("/user", userRoutes)
 app.use("/posts", postsRoutes)
