44f97a1826603033837051dcf72b7ab7b88d29be
ivica.kustura@rathmann.hr <kuuste@gmail.com>
2019-08-23 09:55:40 +0200

    c

--- api/models/user.js
+++ api/models/user.js
@@ -11,7 +11,7 @@ const userSchema = mongoose.Schema({
     match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
   },
   password: { type: String, required: true },
-  dateRegisterd: { type: Date },
+  dateRegistered: { type: Date },
   lastLogin: { type: Date }
 })
 
